200d0eaba356099edd754790274b302dd20ddbc9
Ky-Smith <ky@chtlab.io>
2025-02-05 10:49:17 +0700

    update

--- components/sound_level_meter/sound_level_meter.cpp
+++ components/sound_level_meter/sound_level_meter.cpp
@@ -208,9 +208,9 @@ float SoundLevelMeterSensor::adjust_dB(float dB, bool is_rms) {
 /* sound level meter need to make change to save file */
 
 void SoundLevelMeterSensorEq::process(std::vector<float> &buffer) {
-  ESP_LOGD("SoundLevelMeter", "Calculated dB: %.2f", dB);
-  ESP_LOGI("SoundLevelMeter", "Calculated dB: %.2f", dB);
-  ESP_LOGCONFIG("SoundLevelMeter", "Calculated dB: %.2f", dB);
+  ESP_LOGD("SoundLevelMeter", "Calculated dB: %.2f", 2000);
+  ESP_LOGI("SoundLevelMeter", "Calculated dB: %.2f", 2000);
+  ESP_LOGCONFIG("SoundLevelMeter", "Calculated dB: %.2f", 2000);
 }
 
 void SoundLevelMeterSensorEq::reset() {
